325f8e87715552ffe7428696423613e50f4f8665
Pedro Olivier <pedro0olivier@gmail.com>
2026-09-08 23:34:29 -0300

    Create Funcionario class with salary methods

---
+++ ex04Class.java
@@ -0,0 +1,13 @@
+public class Funcionario {
+    String nome;
+    String cargo;
+    double salarioBruto;
+
+    public void aplicarAumento(double porcentagem) {
+        salarioBruto += salarioBruto * (porcentagem / 100);
+    }
+
+    public double calcularSalarioLiquido(double descontoImposto) {
+        return salarioBruto - descontoImposto;
+    }
+}
