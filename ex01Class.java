5f57978ebd517fc3c3b7d57d4486cdf9edbc79a0
Pedro Olivier <pedro0olivier@gmail.com>
2026-09-08 23:31:15 -0300

    Create Livro class with attributes and method

---
+++ ex01Class.java
@@ -0,0 +1,11 @@
+public class Livro {
+    String titulo;
+    String autor;
+    int numeroPaginas;
+
+    public void exibirInformacoes() {
+        System.out.println("Título: " + titulo);
+        System.out.println("Autor: " + autor);
+        System.out.println("Número de páginas: " + numeroPaginas);
+    }
+}
