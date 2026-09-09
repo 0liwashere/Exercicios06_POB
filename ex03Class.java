927597a68c6942b57ea8addb8dce39480f633e75
Pedro Olivier <pedro0olivier@gmail.com>
2026-09-08 23:33:42 -0300

    Implement ContaBancaria class with basic methods

---
+++ ex03Class.java
@@ -0,0 +1,23 @@
+public class ContaBancaria {
+    String titular;
+    String numeroConta;
+    double saldo = 0;
+
+    public void depositar(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+        }
+    }
+
+    public void sacar(double valor) {
+        if (valor > 0 && saldo >= valor) {
+            saldo -= valor;
+        } else {
+            System.out.println("Saldo insuficiente ou valor inválido");
+        }
+    }
+
+    public void consultarSaldo() {
+        System.out.println("Saldo atual: " + saldo);
+    }
+}
